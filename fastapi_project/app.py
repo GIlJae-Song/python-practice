434ee424e950dac8f11376ba9951ddf8e864d97c
GilJae Song <gjsonofg@gmail.com>
2025-02-22 20:58:30 +0900

    feat: 기본 FastAPI 엔드포인트 및 사용자 생성 API 추가
    - "/item/{item_id}" 엔드포인트: 경로 매개변수를 통한 아이템 조회 기능 추가
    - "/items/" 엔드포인트: 쿼리 매개변수를 통한 아이템 리스트 조회 기능 추가
    - "/user/" POST 엔드포인트: Pydantic 모델을 이용한 사용자 생성 API 구현

--- fastapi_project/app.py
+++ fastapi_project/app.py
@@ -1,11 +1,29 @@
 # FastAPI 모듈에서 FastAPI 클래스를 가져옵니다.
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI() #FastAPI 클래스의 인스턴스를 생성합니다.
 
 @app.get("/")   # @app.get("/")는 "/": 루트 URL에 오는 GET 요청을 처리하겠다는 의미입니다.
-def read_root():
+async def read_root():
     return {"Hello":"World"}
+
+@app.get("/item/{item_id}")  # 첫번째 파라미터 삽입. 경로 매개변수
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/items/")  # 두번쨰 파라미터. 쿼리 매개변수
+async def read_items(item_1 : int = 1, item_2 : int = 2):
+    return {"items" : [item_1,item_2]}
+
+class User(BaseModel):
+    username: str
+    age: int
+    password: str
+
+@app.post("/user/")
+async def create_user(user: User):
+    return {"user": user.dump()}
 
 # 만약 이 파일을 직접 실행하면, 아래 코드를 실행하게 됩니다.
 # 예를 들어, 'python app.py'로 실행할 때만 이 코드가 실행됩니다.
@@ -17,3 +35,7 @@ if __name__ == "__main__":
     # host = 'localhost'는 서버를 로컬호스트에서 실행하고,
     # port = 8000은 8000번 포트를 사용하여 서버가 실행되도록 합니다.
     uvicorn.run(app, host = 'localhost', port = 8000)
+
+
+
+
